b7bf53bba91291b527350266678af18a6c6b6d29
Tim5322 <tjwr.hoevenaar@student.avans.nl>
2025-03-10 15:52:58 +0100

    user.cs deleted

--- ProjectMap.WebApi/Models/Environment2D.cs
+++ ProjectMap.WebApi/Models/Environment2D.cs
@@ -5,7 +5,6 @@
         public Guid Id { get; set; }
         public string? Name { get; set; }
         public Guid UserId { get; set; }
-        public User? User { get; set; }
         public ICollection<Object2D>? Objects { get; set; }
     }
 }
