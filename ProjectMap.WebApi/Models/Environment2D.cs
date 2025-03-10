794d2a7fa073671c6276e7d427921ee38fa6cd3c
Tim5322 <tjwr.hoevenaar@student.avans.nl>
2025-03-10 13:38:16 +0100

    deleted maxheight and maxwidth

--- ProjectMap.WebApi/Models/Environment2D.cs
+++ ProjectMap.WebApi/Models/Environment2D.cs
@@ -4,8 +4,6 @@
     {
         public Guid Id { get; set; }
         public string? Name { get; set; }
-        public int MaxHeight { get; set; }
-        public int MaxLength { get; set; }
         public Guid UserId { get; set; }
         public User? User { get; set; }
         public ICollection<Object2D>? Objects { get; set; }
