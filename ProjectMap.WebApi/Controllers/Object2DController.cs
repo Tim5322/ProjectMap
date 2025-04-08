59b732bdf6f08f5ed92018b0cdafa6cdb8798401
Tim Hoevenaar <tjwr.hoevenaar@student.avans.nl>
2025-04-08 20:55:01 +0200

    kutapi werkt eindelijk

--- ProjectMap.WebApi/Controllers/Object2DController.cs
+++ ProjectMap.WebApi/Controllers/Object2DController.cs
@@ -1,73 +1,63 @@
-using Microsoft.AspNetCore.Mvc;
-using ProjectMap.WebApi.Models;
-using ProjectMap.WebApi.Repositories;
-using System;
+//using Microsoft.AspNetCore.Mvc;
+//using ProjectMap.WebApi.Models;
+//using ProjectMap.WebApi.Repositories;
+//using System;
 
-namespace ProjectMap.WebApi.Controllers;
+//namespace ProjectMap.WebApi.Controllers;
 
-[ApiController]
-[Route("Object2Ds")]
-public class Object2DController : ControllerBase
-{
-    private readonly Object2DRepository _object2DRepository;
-    private readonly ILogger<Object2DController> _logger;
+//[ApiController]
+//[Route("Object2Ds")]
+//public class Object2DController : ControllerBase
+//{
+//    private readonly Object2DRepository _object2DRepository;
+//    private readonly ILogger<Object2DController> _logger;
 
-    public Object2DController(Object2DRepository object2DRepository, ILogger<Object2DController> logger)
-    {
-        _object2DRepository = object2DRepository;
-        _logger = logger;
-    }
+//    public Object2DController(Object2DRepository object2DRepository, ILogger<Object2DController> logger)
+//    {
+//        _object2DRepository = object2DRepository;
+//        _logger = logger;
+//    }
 
-    [HttpGet("{environment2DId}", Name = "ReadObject2Ds")]
-    public async Task<ActionResult<IEnumerable<Object2D>>> Get(Guid environment2DId)
-    {
-        var object2Ds = await _object2DRepository.ReadAsync(environment2DId);
-        return Ok(object2Ds);
-    }
+//    [HttpGet("{environment2DId}", Name = "ReadObject2Ds")]
+//    public async Task<ActionResult<IEnumerable<Object2D>>> Get(Guid environment2DId)
+//    {
+//        var object2Ds = await _object2DRepository.ReadAsync(environment2DId);
+//        return Ok(object2Ds);
+//    }
 
-    [HttpGet("{object2DId}", Name = "ReadObject2D")]
-    public async Task<ActionResult<Object2D>> Get(Guid object2DId)
-    {
-        var object2D = await _object2DRepository.ReadAsync(object2DId);
-        if (object2D == null)
-            return NotFound();
+//    [HttpPost(Name = "CreateObject2D")]
+//    public async Task<ActionResult> Add(Object2D object2D)
+//    {
+//        object2D.Id = Guid.NewGuid();
 
-        return Ok(object2D);
-    }
+//        var createdObject2D = await _object2DRepository.InsertAsync(object2D);
+//        return CreatedAtRoute("ReadObject2D", new { object2DId = createdObject2D.Id }, createdObject2D);
+//    }
 
-    [HttpPost(Name = "CreateObject2D")]
-    public async Task<ActionResult> Add(Object2D object2D)
-    {
-        object2D.Id = Guid.NewGuid();
+//    [HttpPut("{object2DId}", Name = "UpdateObject2D")]
+//    public async Task<ActionResult> Update(Guid object2DId, Object2D newObject2D)
+//    {
+//        var existingObject2D = await _object2DRepository.ReadAsync(object2DId);
 
-        var createdObject2D = await _object2DRepository.InsertAsync(object2D);
-        return CreatedAtRoute("ReadObject2D", new { object2DId = createdObject2D.Id }, createdObject2D);
-    }
+//        if (existingObject2D == null)
+//            return NotFound();
 
-    [HttpPut("{object2DId}", Name = "UpdateObject2D")]
-    public async Task<ActionResult> Update(Guid object2DId, Object2D newObject2D)
-    {
-        var existingObject2D = await _object2DRepository.ReadAsync(object2DId);
+//        newObject2D.Id = object2DId;
+//        await _object2DRepository.UpdateAsync(newObject2D);
 
-        if (existingObject2D == null)
-            return NotFound();
+//        return Ok(newObject2D);
+//    }
 
-        newObject2D.Id = object2DId;
-        await _object2DRepository.UpdateAsync(newObject2D);
+//    [HttpDelete("{object2DId}", Name = "DeleteObject2D")]
+//    public async Task<IActionResult> Delete(Guid object2DId)
+//    {
+//        var existingObject2D = await _object2DRepository.ReadAsync(object2DId);
 
-        return Ok(newObject2D);
-    }
+//        if (existingObject2D == null)
+//            return NotFound();
 
-    [HttpDelete("{object2DId}", Name = "DeleteObject2D")]
-    public async Task<IActionResult> Delete(Guid object2DId)
-    {
-        var existingObject2D = await _object2DRepository.ReadAsync(object2DId);
+//        await _object2DRepository.DeleteAsync(object2DId);
 
-        if (existingObject2D == null)
-            return NotFound();
-
-        await _object2DRepository.DeleteAsync(object2DId);
-
-        return Ok();
-    }
-}
+//        return Ok();
+//    }
+//}
--- ProjectMap.WebApi/Program.cs
+++ ProjectMap.WebApi/Program.cs
@@ -15,7 +15,8 @@ builder.Services.Configure<RouteOptions>(o => o.LowercaseUrls = true);
 var sqlConnectionString = builder.Configuration.GetValue<string>("SqlConnectionString");
 var sqlConnectionStringFound = !string.IsNullOrWhiteSpace(sqlConnectionString);
 
-builder.Services.AddTransient<Object2DRepository, Object2DRepository>(o => new Object2DRepository(sqlConnectionString));
+builder.Services.AddTransient<IObject2DRepository, Object2DRepository>(o => new Object2DRepository(sqlConnectionString));
+builder.Services.AddTransient<IEnvironment2DRepository, Environment2DRepository>(o => new Environment2DRepository(sqlConnectionString));
 
 builder.Services.AddAuthorization();
 builder.Services
@@ -24,6 +25,11 @@ builder.Services
     {
         options.ConnectionString = sqlConnectionString;
     });
+
+// Adding the HTTP Context accessor to be injected. This is needed by the AspNetIdentityUserRepository
+// to resolve the current user.
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddTransient<IAuthenticationService, AspNetIdentityAuthenticationService>();
 
 var app = builder.Build();
 
